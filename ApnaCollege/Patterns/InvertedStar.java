e8c874a8efedc05f86c4a3015115bb38e1513ec5
Ali Munir <alimunirwaris1@gmail.com>
2024-05-17 10:00:00 +0500

    working with inverted star pattern in java

---
+++ ApnaCollege/Patterns/InvertedStar.java
@@ -0,0 +1,15 @@
+package ApnaCollege.Patterns;
+
+public class InvertedStar {
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
